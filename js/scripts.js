77a5fbdd54d114e6d1996a20a817f56430404731
Fedois <fedefox14@gmail.com>
2023-01-24 14:53:47 +0100

    aggiunta logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        inputText: '',
+        line: '',
+        done: false,
+        list: ['ciao', 'come', 'stai'],
+      }
+    },
+    methods: {
+        addText(){
+            if(this.inputText != ''){
+                this.list.push(this.inputText)
+                console.log(this.list)
+            }
+            
+            this.inputText = ''
+        },
+        removeList(index){
+            this.list.splice(index, 1);
+        },
+    },
+  }).mount('#app')
